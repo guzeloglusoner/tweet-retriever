4ed2ff85482e2f11c6d67d52bbef1c0282441171
guzeloglusoner <guzeloglusoner@gmail.com>
2019-05-17 10:26:46 +0300

    Initial Commit

---
+++ build.sbt
@@ -0,0 +1,22 @@
+name := "TwitterAnalysis"
+
+version := "0.1"
+
+scalaVersion := "2.11.0"
+
+// https://mvnrepository.com/artifact/org.apache.spark/spark-core
+libraryDependencies += "org.apache.spark" %% "spark-core" % "2.3.2"
+
+// https://mvnrepository.com/artifact/org.apache.spark/spark-streaming
+libraryDependencies += "org.apache.spark" %% "spark-streaming" % "2.3.2"
+
+// https://mvnrepository.com/artifact/org.apache.bahir/spark-streaming-twitter
+libraryDependencies += "org.apache.bahir" %% "spark-streaming-twitter" % "2.3.2"
+
+// https://mvnrepository.com/artifact/net.liftweb/lift-json
+libraryDependencies += "net.liftweb" %% "lift-json" % "3.3.0"
+
+assemblyMergeStrategy in assembly := {
+  case PathList("META-INF", xs @ _*) => MergeStrategy.discard
+  case x => MergeStrategy.first
+}
---
+++ src/main/scala/org/apache/spark/examples/streaming/twitter/TwitterLocations.scala
@@ -0,0 +1,67 @@
+package org.apache.spark.examples.streaming.twitter
+
+import org.apache.spark.SparkConf
+import org.apache.spark.streaming.twitter.TwitterUtils
+import org.apache.spark.streaming.{Seconds, StreamingContext}
+import twitter4j.FilterQuery
+import scala.io.Source
+import net.liftweb.json._
+
+
+/* Illustrates the use of custom filter queries to get Tweets from one or more locations. */
+object TwitterLocations {
+  implicit val formats: DefaultFormats.type = net.liftweb.json.DefaultFormats
+  case class TwitterCredentials(consumerKey: String, consumerSecret: String, accessToken: String, accessTokenSecret: String)
+
+  def main(args: Array[String]): Unit = {
+
+    /* Read the file */
+    var str = ""
+    for (line <- Source.fromFile("C:\\DEV\\Projects\\TwitterAnalysis\\src\\resources\\env.json").getLines) str += line
+
+    /* Map the JSON file into a case class */
+    val twitterCredentials = parse(str).json.extract[TwitterCredentials]
+
+    /* Set the system properties so that Twitter4j library used by twitter stream */
+    System.setProperty("twitter4j.oauth.consumerKey", twitterCredentials.consumerKey)
+    System.setProperty("twitter4j.oauth.consumerSecret", twitterCredentials.consumerSecret)
+    System.setProperty("twitter4j.oauth.accessToken", twitterCredentials.accessToken)
+    System.setProperty("twitter4j.oauth.accessTokenSecret", twitterCredentials.accessTokenSecret)
+
+    /* Get bounding boxes of locations for which to retrieve Tweets from command line */
+    val boundingBoxes = {
+      val southWest = Array(25.62128903, 35.80768033)
+      val northEast = Array(44.81766374, 42.29699998)
+
+      Array(southWest, northEast)
+    }
+
+    val sparkConf = new SparkConf().setAppName("TwitterLocations")
+
+    /* Sets spark master to local */
+    if (!sparkConf.contains("spark.master")) {
+      sparkConf.setMaster("local[*]")
+    }
+
+    val ssc: StreamingContext = new StreamingContext(sparkConf, Seconds(1))
+    ssc.sparkContext.setLogLevel("ERROR")
+
+    val locationsQuery: FilterQuery = new FilterQuery().locations(boundingBoxes: _*)
+
+    /* Print the tweets with given coordinates*/
+    TwitterUtils.createFilteredStream(ssc, None, Some(locationsQuery))
+      .map(tweet => {
+
+        val latitude = Option(tweet.getGeoLocation).map(l => s"${l.getLatitude},${l.getLongitude}")
+        val place = Option(tweet.getPlace).map(_.getName)
+        val location = latitude.getOrElse(place.getOrElse("(no location)"))
+        val text = tweet.getText.replace('\n', ' ').replace('\r', ' ').replace('\t', ' ')
+        val user: Option[String] = Option(tweet.getUser).map(u => s"${u.getName}")
+        s"Username: ${user.getOrElse("testUser")}\n Location: $location\n Tweet:$text\n"
+      }).print()
+
+    ssc.start()
+    ssc.awaitTermination()
+
+  }
+}
